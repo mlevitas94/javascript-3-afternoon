d5eeb6a98fb076b1e660889a578c35ec9ec105f1
Mark94 <mlevitas94@gmail.com>
2019-01-09 15:58:17 -0700

    first two javascripts finished

--- classes.js
+++ classes.js
@@ -29,7 +29,18 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`;
+  }
+}
 
 
 
@@ -49,7 +60,28 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager {
+  constructor(first_name, last_name, email, age,){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = []
+    
+  }
+
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`;
+  }
+
+  hire(employee){
+    this.reports.push(employee)
+  }
+
+  fire(index){
+    this.reports.splice(index,1)
+  }
+}
 
 
 
@@ -75,7 +107,59 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager {
+  constructor(first_name, last_name, email, age,){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = []
+    this.title = "Not a manager";
+    this.bonus = 0;
+    
+  }
+
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`;
+  }
+
+  hire(employee){
+    this.reports.push(employee)
+    if(this.reports.length === 0){
+      this.title = "Not a manager";
+    }else if(this.reports.length > 0 && this.reports.length < 4){
+      this.title = "Barely Manager";
+    }else if(this.reports.length > 3 && this.reports.length < 11){
+      this.title = "Mostly Manager";
+    }else if(this.reports.length > 10 && this.reports.length < 51){
+      this.title = "Manager";
+    }else if(this.reports.length > 50 && this.reports.length < 101){
+      this.title = "Manager Plus";
+    }else{
+      this.title = "Bestest Manager";
+    }
+    
+  }
+
+  fire(index){
+    this.reports.splice(index,1)
+    if(this.reports.length === 0){
+      this.title = "Not a manager";
+    }else if(this.reports.length > 0 && this.reports.length < 4){
+      this.title = "Barely Manager";
+    }else if(this.reports.length > 3 && this.reports.length < 11){
+      this.title = "Mostly Manager";
+    }else if(this.reports.length > 10 && this.reports.length < 51){
+      this.title = "Manager";
+    }else if(this.reports.length > 50 && this.reports.length < 101){
+      this.title = "Manager Plus";
+    }else{
+      this.title = "Bestest Manager";
+    }
+    this.bonus+= 100;
+  }
+}
+
 
 
 
@@ -102,6 +186,31 @@
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
+class Machine {
+  constructor(){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false
+  }
+
+  makeWidgets(num){
+    this.widgets_made_count += num;
+    this.wear_and_tear_count = this.widgets_made_count / 50;
+  }
+
+  fixMachine(){
+    this.needs_reboot = true;
+  }
+
+  reboot(){
+    return () => {
+      if(this.needs_reboot === true){
+        this.wear_and_tear_count -= 10;
+        this.needs_reboot = false;
+      }
+    }
+  
+  }
+}
 
 
--- destructuring.js
+++ destructuring.js
@@ -21,7 +21,7 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
+var {color, make, model, year} = carDetails;
 
 
 
@@ -34,7 +34,7 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+  var {firstName, lastName, title} = obj;
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -53,7 +53,11 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
+function totalPopulation(obj){
+  var {utah, california, texas, arizona} = obj
+
+  return utah + california + texas + arizona
+}
 
 
 
@@ -67,7 +71,19 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
+function ingredients(obj){
+  var {carb, fat, protein} = obj;
+  var carb = carb;
+  var fat = fat;
+  var protein = protein;
+
+  var arr = [];
+
+  arr.push(carb);
+  arr.push(fat);
+  arr.push(protein);
+  return arr
+}
 
 
 
@@ -85,7 +101,9 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
+function largeNumbers( {first, second, third}){
+  return Math.min(first, second, third)
+}
 
 
 
@@ -97,6 +115,13 @@ function greeting( obj ) {
   Find the longest array and return that array.
 */
 
-//Code Here
+function numberGroups({a, b, c}){
+  if(a.length > b.length && a.length > c.length){
+    return a
+  } else if (b.length > a.length && b.length > c.length ){
+    return b
+  }else{
+    return c
+  }
 
-
+}
