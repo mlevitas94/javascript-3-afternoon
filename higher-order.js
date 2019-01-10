1219149177494be5fb782e7e6933ab6ed7bf8c97
Mark94 <mlevitas94@gmail.com>
2019-01-09 20:56:34 -0700

    finished fourth javascript

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,7 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers  = mixedNumbers.filter((ele) => ele % 2 ===0)
 
 
 
@@ -44,7 +44,8 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices  = prices.map((ele) => ele * 1.07
+ );
 
 
 
@@ -63,7 +64,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation  = populations.reduce((total, ele) =>  total+= ele)
 
 
 
@@ -89,7 +90,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter((item) => item["CP"] > 200)
 
 
 
@@ -106,7 +107,11 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map((item) => {
+var taxed = item["tax"] + 1;
+item = item["price"] * taxed;
+  return item
+})
 
 
 
@@ -126,6 +131,12 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce((total, item) =>{ 
+  if(item["owner"] === "Bob"){
+    total += item["price"];
+  }
+console.log(total)
+  return total
+},0)
 
 
